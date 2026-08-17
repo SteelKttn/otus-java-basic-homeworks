66a09c9775683ab23c937cd4dde0915c33447f7c
RobotComp.ru <egorshilov0@gmail.com>
2026-08-17 21:58:38 +0300

    init commit

--- src/main/java/ru/otus/java/basic/homeworks/HomeworksApplication.java
+++ src/main/java/ru/otus/java/basic/homeworks/HomeworksApplication.java
@@ -4,65 +4,8 @@ public class HomeworksApplication {
 
 
     public static void main(String[] args) {
-        greetings();
-        checkSign();
-        selectColor();
-        compareNumbers();
-        addOrSubtractAndPrint(2,4, false);
     }
-        public static void greetings() {
-            System.out.println("Hello");
-            System.out.println("World");
-            System.out.println("from");
-            System.out.println("Java");
-        }
-        public static void checkSign() {
-            int a = 21;
-            int b = 32;
-            int c = 47;
-            if (a + b + c >= 0) {
-                System.out.println("Сумма положительная");
-            } else {
-                System.out.println("Сумма отрицательная");
-            }
-        }
-        public static void selectColor() {
-                int data = 48;
-                if (data < 10) {
-                    System.out.println("Красный");
-                }
-                if (data >= 10 && data <= 20) {
-                    System.out.println("Желтый");
-                }
-                if (data > 20) {
-                    System.out.println("Зеленый");
-                }
-            }
-            public static void compareNumbers() {
-        int a = 12;
-        int b = 8;
-        if (a>=b) {
-            System.out.println("a >= b");
-        } else {
-            System.out.println("a < b");
-        }
-            }
-            public static void addOrSubtractAndPrint(int initValue, int delta, boolean increment) {
-
-                if (increment == true) {
-                    int result = initValue + delta;
-                    System.out.println(result);
-                } else {
-                    int result = initValue - delta;
-                    System.out.println(result);
-                }
-            }
-
-
-
-
-        }
-
+}
 
 
 
