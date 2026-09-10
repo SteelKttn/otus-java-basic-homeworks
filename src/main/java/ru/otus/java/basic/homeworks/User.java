3b4ee29051e9e10dbf9c91e3bd0e5e52d26607e3
RobotComp.ru <egorshilov0@gmail.com>
2026-09-10 15:42:43 +0300

    выполнил ДЗ №4

---
+++ src/main/java/ru/otus/java/basic/homeworks/User.java
@@ -0,0 +1,29 @@
+package ru.otus.java.basic.homeworks;
+
+public class User {
+    public String name;
+    public String patronymic;
+    public String surname;
+    public int yearOfBirth;
+    public String email;
+
+
+    public User(String surname, String name, String patronymic, int yearOfBirth, String email) {
+        this.patronymic = patronymic;
+        this.name = name;
+        this.surname = surname;
+        this.yearOfBirth = yearOfBirth;
+        this.email = email;
+
+    }
+
+
+
+    public void info() {
+        System.out.println("ФИО: " + surname + " " + name + " " + patronymic + "\n" + "Год рождения: " + yearOfBirth + "\n" + "e-mail: " + email);
+
+
+
+    }
+
+}
